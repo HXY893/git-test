e9aaf8a6bf8f6c535df4d48848dbb64bd00bc7c1
HXY893 <356696465@qq.com>
2018-12-14 10:18:48 +0800

    update user

--- src/main/java/com/qf/pojo/po/user.java
+++ src/main/java/com/qf/pojo/po/user.java
@@ -2,8 +2,7 @@ package com.qf.pojo.po;
 
 public class user {
     private int id;
-    private String  name;
-
+    private int age;
     public int getId() {
         return id;
     }
@@ -12,11 +11,11 @@ public class user {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public int getAge() {
+        return age;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setAge(int age) {
+        this.age = age;
     }
 }
